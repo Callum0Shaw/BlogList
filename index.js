9919165bcc5d99b63009b7e4b748a5d0feeda856
Your Name <callum0shaw.gmail.com>
2021-02-26 12:27:59 +0200

    SUBMIT: Exercise 4.2

--- index.js
+++ index.js
@@ -1,61 +1,10 @@
-const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const app = require('./app') // the actual Express application
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const server = http.createServer(app)
 
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => console.log("connected to MongoDB"))
-  .catch((error) =>
-    console.log(`Failed to connect to MongoDB: ${error.message}`)
-  );
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((error) => console.log(`Failed to get blogs: ${error.message}`));
-});
-
-app.post("/api/blogs", (request, response) => {
-  const body = request.body;
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  });
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => console.log(`Failed ot post new blog: ${error.message}`));
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
