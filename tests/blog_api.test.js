855a1d615e3fcabc737f38931d7b22dc52671fc3
Your Name <callum0shaw.gmail.com>
2021-03-04 15:06:01 +0200

    SUBMIT: Exercise 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,45 +1,32 @@
-const mongoose = require('mongoose');
-const supertest = require('supertest');
-const app = require('../app');
-const api = supertest(app)
-const Blog = require('../models/blogs');
-
-let initialBlogs = [
-  {
-    title: "Blog 1",
-    author: "Author 1",
-    url: "url 1",
-    likes: 1,
-  },
-  {
-    title: "Blog 2",
-    author: "Author 2",
-    url: "url 2",
-    likes: 2,
-  },
-  {
-    title: "Blog 3",
-    author: "Author 3",
-    url: "url 3",
-    likes: 3,
-  },
-]
-
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const Blog = require("../models/blogs");
+const helper = require("./test_helper");
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
- 
-  const blogObjects = initialBlogs.map(blog => new Blog(blog))
-  
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
+  await Blog.deleteMany({});
 
-})
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
 
 test("Verify app gets correct number of blogs in JSON", async () => {
-  await api.get("/api/blogs").expect(200).expect("Content-Type", /application\/json/)
-})
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("Verify that unique identifer 'id' exists", async () => {
+  const blogs = await helper.blogsInDb();
+
+  expect(blogs[0].id).toBeDefined();
+});
 
 afterAll(() => {
-  mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
